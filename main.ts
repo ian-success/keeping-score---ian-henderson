06d9b750e464d00883ce899d5ad21055184db576
ian-success <112981850+ian-success@users.noreply.github.com>
2022-09-06 13:53:48 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(Students)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(Tie)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(Teachers)
+})
+let Students = 0
+let Teachers = 0
+let Tie = 0
+Tie = 0
+Teachers = 0
+Students = 0
