716f2cd045a7a03f3eca86be75f32e538e7f9187
ian-success <112981850+ian-success@users.noreply.github.com>
2022-09-06 14:00:02 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,56 @@
 input.onButtonPressed(Button.A, function () {
     basic.showNumber(Students)
+    Students += 1
+    basic.showLeds(`
+        # # # # #
+        # . . . .
+        # # # # #
+        . . . . #
+        # # # # #
+        `)
+    basic.clearScreen()
 })
 input.onButtonPressed(Button.AB, function () {
     basic.showNumber(Tie)
+    Tie += 1
+    basic.showLeds(`
+        # # # # #
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    basic.showLeds(`
+        # # # # #
+        . . # . .
+        . . # . .
+        . . # . .
+        # # # # #
+        `)
+    basic.clearScreen()
 })
 input.onButtonPressed(Button.B, function () {
     basic.showNumber(Teachers)
+    Teachers += 1
+    basic.showLeds(`
+        # # # # #
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    basic.showLeds(`
+        # # # # #
+        # . . . .
+        # # # # #
+        # . . . .
+        # # # # #
+        `)
+    basic.clearScreen()
 })
 let Students = 0
 let Teachers = 0
 let Tie = 0
-Tie = 0
-Teachers = 0
-Students = 0
+Tie = 1
+Teachers = 1
+Students = 1
